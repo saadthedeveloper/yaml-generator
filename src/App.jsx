8dddd87486c97b14bbacd2e2eb9e9b45bafffd71
saadthedeveloper <m.saad23145@gmail.com>
2026-02-03 20:14:17 +0500

    Add basic application layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
 import { useState } from 'react'
 import './App.css'
-import ElasticSearch from './components/ElasticSearch.jsx'
 
 
 function App() {
 
+  let [userChoice, setUserChoice] = useState();
+  const userChoiceHandle = (event) => {
+    if (event.target.checked) {
+      setUserChoice("Enabled");
+    } else {
+      setUserChoice("Disabled");
+    }
+  }
+
+
   return (
-    <div>
-      <ElasticSearch/>
-    </div>
+    <main className="config-app">
+      <header>
+        <p>Step 1 Of 5</p>
+      </header>
+
+      <section className="question-section">
+        <h2>Enable Elastic Search</h2>
+        <div class="container">
+          <label class="switch" for="checkbox">
+            <input type="checkbox" id="checkbox" onChange={userChoiceHandle}/>
+            <div class="slider round"></div>
+          </label>
+        </div>
+      </section>
+
+      <nav className='app-navigation'>
+        <button className="nav-button back-button">Back</button>
+        <button className="nav-button next-button">Next</button>
+      </nav>
+
+      <section>
+        <p>{userChoice}</p>
+      </section>
+
+    </main>
   )
 }
 
